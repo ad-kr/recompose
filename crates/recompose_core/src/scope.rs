b20de09fd096635e893e43ec282fe9acda837a0b
adrian <bounce.adrian@gmail.com>
2025-01-14 19:21:36 +0100

    Add documentation to the `Scope`

--- crates/recompose_core/src/scope.rs
+++ crates/recompose_core/src/scope.rs
@@ -11,18 +11,39 @@ use std::{any::Any, collections::HashMap, sync::Arc};
 #[derive(Default, Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct ScopeId(usize);
 
+/// A scope can be thought of as a "sum" of all modifications done by the [`compose`](crate::Compose::compose) function
+/// of the [`Compose`](crate::Compose) trait. It holds the state of the composable and its children scopes. It is the
+/// "actual" node in the tree-structure of the composables.
 pub struct Scope<'a> {
     pub(crate) id: ScopeId,
 
     /// Indicates which index in the parent's children vector this scope is.
     pub(crate) index: usize,
+
+    /// For composables that spawn an entity, this is the field that holds the rerefence to the entity.
     pub(crate) entity: Option<Entity>,
+
+    /// The parent scope of this scope. Root-scopes do not have parents.
     pub(crate) parent: Option<ScopeId>,
+
+    /// Indicates if the scope will decompose on before the next recomposition.
     pub(crate) will_decompose: bool,
+
+    /// A copy of the composer that created this scope. It is used to recompose this scope when one of the states was
+    /// changed, but the parent scope was recomposed.
     pub(crate) composer: Arc<dyn AnyCompose + 'a>,
+
+    /// The index counter of the states when running the `compose` function. It is used to keep track of the states.
     pub(crate) state_index: usize,
+
+    /// The states of the composable.
     pub(crate) states: Vec<DynState>,
+
+    /// The children of the composable.
     pub(crate) children: Vec<Scope<'a>>,
+
+    /// The "collected" systems after the `compose`-function was executed. The systems are run and discarded after the
+    /// recomposition.
     pub(crate) queued_systems: Vec<BoxedSystem<(), ()>>,
 }
 
@@ -74,6 +95,8 @@ impl Scope<'_> {
         }
     }
 
+    /// Creates a new state. States are persisted between each recomposition of the composable. Each time a state
+    /// changes, the scope it belongs to is schedules for recomposition.
     pub fn use_state<T: Any + Send + Sync>(&mut self, initial_value: T) -> State<T> {
         if let Some(existing_state) = self.states.get(self.state_index) {
             self.state_index += 1;
@@ -96,6 +119,8 @@ impl Scope<'_> {
         state
     }
 
+    /// Creates a new state with a given id. It is useful for cases where you want to reference a state in an external
+    /// system or composable.
     pub fn use_state_with_id<T: Any + Send + Sync>(
         &mut self,
         state_id: TypedStateId<T>,
@@ -122,6 +147,7 @@ impl Scope<'_> {
         state
     }
 
+    /// Sets the value of the given state. The change happens immediately.
     pub fn set_state<T: Send + Sync + 'static>(&mut self, state: &State<T>, value: T) {
         let state = self
             .states
@@ -137,6 +163,7 @@ impl Scope<'_> {
         state.changed = StateChanged::Queued;
     }
 
+    /// Sets the value of a state with a given id. The change happens immediately.
     pub fn set_state_with_id<T: Send + Sync + 'static>(
         &mut self,
         state_id: TypedStateId<T>,
@@ -165,6 +192,7 @@ impl Scope<'_> {
         dyn_state.to_state()
     }
 
+    /// A callback that is run only if the dependencies have changed.
     pub fn use_effect(&mut self, effect: impl Fn(), dependecies: impl Dependency) {
         if !dependecies.has_changed() {
             return;
@@ -173,17 +201,20 @@ impl Scope<'_> {
         effect();
     }
 
+    /// Runs a callback when the component is first composed.
     pub fn use_mount(&mut self, callback: impl Fn()) {
         let once = self.use_state(());
         self.use_effect(callback, once);
     }
 
-    // TODO: Add description about that it is not cached
+    /// Runs a system. The system is not cached and is "rebuilt" every time the composable recomposes. It is therefore
+    /// not the most efficient way to to interact with the ECS world.
     pub fn use_system<M>(&mut self, system: impl IntoSystem<(), (), M>) {
         let sys: BoxedSystem<(), ()> = Box::from(IntoSystem::into_system(system));
         self.queued_systems.push(sys);
     }
 
+    /// Runs a system when the composable is first composed.
     pub fn use_system_once<M>(&mut self, system: impl IntoSystem<(), (), M>) {
         let once = self.use_state(());
 
@@ -192,11 +223,11 @@ impl Scope<'_> {
         }
     }
 
-    pub fn get_parent(&self) -> Option<ScopeId> {
+    pub(crate) fn get_parent(&self) -> Option<ScopeId> {
         self.parent
     }
 
-    pub fn set_entity(&mut self, entity: Entity) {
+    pub(crate) fn set_entity(&mut self, entity: Entity) {
         self.entity = Some(entity);
     }
 
